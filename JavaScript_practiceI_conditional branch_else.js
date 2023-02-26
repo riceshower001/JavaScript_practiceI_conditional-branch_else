78cd99555ff17246bfd16e03184ef71087686514
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-26 11:33:08 +0900

    Add files via upload

---
+++ JavaScript_practiceI_conditional branch_else.js
@@ -0,0 +1,8 @@
+const age = 17;
+
+// Please output "I am under 20 years old." when the conditional expression is not satisfied.
+if (age >= 20) {
+  console.log("I am over 20 years old.");
+} else {
+  console.log("I am under 20 years old.");
+}
